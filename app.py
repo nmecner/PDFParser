e22e430a306c82ce04ea532e0a7a288b5a751c09
Natalia Mecner <natalia.mecner@gmail.com>
2017-11-05 23:26:37 +0100

    Added parsing xml feature and extracted text with position coordinates

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from io import BytesIO
+import xml.etree.ElementTree as ET
 app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///pdf.db' #relative path for now
+
 db = SQLAlchemy(app)
 
 
@@ -48,15 +50,35 @@ def pdf_to_text(pdfname):
     return text
 
 
-currentpdf = Pdf(pdf_to_text('sample.pdf'), pdf_to_text('sample.pdf'))
-db.create_all()
-db.session.add(currentpdf)
-db.session.commit()
+# Extracting information from xml parsed by PDFMiner
+
+tree = ET.ElementTree(file='sam.xml')
+root = tree.getroot()
+
+textboxes = [] # list of textboxes
+contents = [] # list of contents of textboxes
+s = '' # string from one textbox
+
+
+# get the coordinates of all the textboxes
+for elm in root.getiterator(tag='textbox'):
+    contents.append(s)
+    s = ''
+    textboxes.append((elm.tag, elm.attrib))
+    for i in elm.getiterator(tag='text'):
+        s += i.text
+
+
+# todo: insert parsed data into dbs
+
+# db.create_all()
+# db.session.add(currentpdf)
+# db.session.commit()
 
 
 @app.route('/')
 def hello_world():
-    return pdf_to_text('sample.pdf')
+    return str(contents)
 
 
 if __name__ == '__main__':
